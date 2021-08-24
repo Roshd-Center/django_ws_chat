32c02921704c243fb6f2a23de5b3672217ed5ecc
mohammadT77 <t.mohammadamin@gmail.com>
2021-08-24 12:36:05 +0430

    Print received messages and setting up ALLOWED_HOSTS

--- django_ws_chat/settings.py
+++ django_ws_chat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)9om3u7n*!w6kwq0%*$he2#7jh-pam9arvo-ltsqsv^!a@tepq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
